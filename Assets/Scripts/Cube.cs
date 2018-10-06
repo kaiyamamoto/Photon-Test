9d51defa111ad4c3e952ba998372cf6730b9035a
やまげんPC <dqx_2527@yahoo.co.jp>
2018-10-06 20:52:31 +0900

    Project追加

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Cube : MonoBehaviour
+{
+	[SerializeField]
+	private float speed = 3.0f;
+
+	private PhotonView photonView;
+
+	void Start()
+	{
+		photonView = GetComponent<PhotonView>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (photonView.IsMine == false) return;
+
+		if (Input.GetKey(KeyCode.UpArrow))
+			transform.position += transform.forward * speed * Time.deltaTime;
+
+		if (Input.GetKey(KeyCode.DownArrow))
+			transform.position -= transform.forward * speed * Time.deltaTime;
+
+		if (Input.GetKey(KeyCode.RightArrow))
+			transform.position += transform.right * speed * Time.deltaTime;
+
+		if (Input.GetKey(KeyCode.LeftArrow))
+			transform.position -= transform.right * speed * Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/PhotonMain.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PhotonMain : MonoBehaviourPunCallbacks
+{
+
+	// Use this for initialization
+	void Start()
+	{
+		PhotonNetwork.ConnectUsingSettings();
+	}
+
+	public override void OnConnectedToMaster()
+	{
+		Debug.Log("接続！");
+		// ランダム入室
+		PhotonNetwork.JoinRandomRoom();
+	}
+
+	public override void OnJoinedRoom()
+	{
+		Debug.Log("部屋参加");
+		float posX = Random.Range(-5, 5);
+		Vector3 pos = new Vector3(posX, 0.0f, 0.0f);
+		PhotonNetwork.Instantiate("Cube", pos, Quaternion.identity, 0);
+	}
+
+	public override void OnJoinRandomFailed(short returnCode, string message)
+	{
+		Debug.Log("部屋入室失敗");
+		PhotonNetwork.CreateRoom(null);
+		Debug.Log("部屋作成");
+	}
+}
